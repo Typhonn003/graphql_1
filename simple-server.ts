9866a1477d907f867f62952c3476a24db6f1425e
Diego <diegolima.almeida93@gmail.com>
2024-01-18 12:21:38 -0300

    feat: query users, mutation createUser

--- simple-server.ts
+++ simple-server.ts
@@ -1,18 +1,35 @@
 import { ApolloServer, gql } from "apollo-server";
 
+type createUser = {
+  name: string;
+};
+
 const typeDefs = gql`
   type Query {
-    helloWorld: String!
+    users: [String!]!
+  }
+
+  type Mutation {
+    createUser(name: String!): String!
   }
 `;
 
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return "Hello World";
+    users: () => {
+      return users;
+    },
+  },
+  Mutation: {
+    createUser: (_: any, { name }: createUser) => {
+      users.push(name);
+
+      return name;
     },
   },
 };
+
+const users: string[] = [];
 
 const server = new ApolloServer({
   typeDefs,
